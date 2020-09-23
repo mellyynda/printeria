dca0e4f5172ee82a503fd2480d373ae959f2f57d
Melinda <melinda.sava.td@gmail.com>
2020-09-23 23:16:57 +0200

    made cover as big as screen height with  javascript

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+ //changes toggle from x to hamburger and back
  function menuButtonEffects() {
      let navBarToggle = document.getElementById('js-navbar-toggle');
      let hamburger = document.getElementById('burger');
@@ -16,6 +17,7 @@
      });
  };
 
+ // opens main nav
  function openMobileNav() {
      let mainNav = document.getElementById('js-menu');
      let navBarToggle = document.getElementById('js-navbar-toggle');
@@ -29,18 +31,9 @@
  menuButtonEffects();
  openMobileNav();
 
- //  let positionOfferCards = function () {
- //      let cardsContainer = document.querySelector('.offers');
-
- //      let screenWidth = window.innerWidth;
 
 
- //      if (screenWidth == 800 || screenWidth >= 1200) {
- //          cardsContainer.style.marginTop = "0";
- //      } else cardsContainer.style.marginTop = "50px";
- //  }
-
- //  positionOfferCards();
+ //  function that gets carousell going from: https://www.w3schools.com/howto/howto_js_slideshow.asp
 
  let slideIndex = 1;
  showSlides(slideIndex);
@@ -74,3 +67,26 @@
      slides[slideIndex - 1].style.display = "block";
      dots[slideIndex - 1].className += " on-dot";
  }
+
+
+ //function that makes cover height as big as screen
+ let makeCoverFull = function () {
+     let nav = document.getElementsByTagName('header');
+     console.log(nav);
+     let navHeight = nav[0].offsetHeight;
+     console.log(navHeight);
+
+     let cover = document.querySelector('.cover');
+     console.log(cover);
+
+     // found this solution to change viewport height to pixels: https://stackoverflow.com/questions/44109314/javascript-calculate-with-viewport-width-height/44109531
+     function vh(v) {
+         var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+         return (v * h) / 100;
+     }
+     console.log(vh(100));
+
+     cover.style.height = ` ${vh(100) - navHeight}px`
+
+ }
+ makeCoverFull();
